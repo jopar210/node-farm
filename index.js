cfc27030cd2e51702d40d363dd16ee0fac94615f
Joel Parra <95077198+jopar210@users.noreply.github.com>
2023-03-27 11:29:15 +0200

    Initial files

---
+++ index.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the OVERVIEW");
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT");
+  } else if (pathName === "/api") {
+    res.writeHead(200, {
+      "Content-type": "application/json",
+    });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "Custom-header": "hello-world",
+    });
+    res.end("Page not found!");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Server running on port 8000");
+});
